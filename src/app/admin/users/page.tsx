42484e2fec81078eb08194dfba5e83b94981bb09
francomd <francomdalmasso@gmail.com>
2024-02-23 20:43:08 -0300

    Add onDelete function for users table

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,38 +1,18 @@
 'use client'
 
-import { Typography } from 'antd';
 const { Title } = Typography;
-import { Table } from 'antd';
+import { Button, Popconfirm, Space, Table, Typography } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import { TUser } from '@/types/User';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { firestore } from '@/services/firebaseService';
-import { collection, getDocs } from 'firebase/firestore';
-import { set } from 'firebase/database';
-
-const columns = [
-  {
-    title: 'Nombre',
-    dataIndex: 'displayName',
-    key: 'displayName',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-  },
-  {
-    title: 'Rol',
-    dataIndex: 'role',
-    key: 'role',
-  }
-];
-
+import { collection, deleteDoc, doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
 
 const UsersPage = () => {
   const [users, setUsers] = useState<TUser[]>([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  const getUsers = () => {
     getDocs(collection(firestore, "users"))
       .then((snapshot) => {
         return snapshot.docs.map((doc) => {
@@ -43,6 +23,69 @@ const UsersPage = () => {
       }).finally(() => {
         setLoading(false);
       });
+  }
+
+  const onDelete = (uid: string, email: string) => {
+    console.log('delete', uid, email);
+    deleteDoc(doc(firestore, "users", uid)).then(() => {
+      getDoc(doc(firestore, "invites", 'list'))
+        .then((snapshot) => {
+          return snapshot.data();
+        }).then((data) => {
+          const updatedInvites = data?.userList.filter((_email: string) => _email !== email);
+          setDoc(doc(firestore, "invites", 'list'), {
+            userList: [...updatedInvites]
+          }).then(() => {
+            getUsers();
+          })
+        })
+    });
+  }
+
+  const columns = useMemo(() => [
+    {
+      title: 'Nombre',
+      dataIndex: 'displayName',
+      key: 'displayName',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Rol',
+      dataIndex: 'role',
+      key: 'role',
+    },
+    {
+      title: 'Acciones',
+      dataIndex: 'uid',
+      key: 'uid',
+      render: (uid: string, { email }: { email: string }) =>
+      (
+        <Space size="middle">
+          <Button type="link">
+            <EditOutlined style={{ color: "black", fontSize: "20px" }} />
+          </Button>
+          <Popconfirm
+            title="Borrar"
+            description="¿Estás seguro de eliminar este usuario?"
+            onConfirm={() => onDelete(uid, email)}
+            okText="Si"
+            cancelText="No"
+          >
+            <Button type="link">
+              <DeleteOutlined style={{ color: 'red', fontSize: "20px" }} />
+            </Button>
+          </Popconfirm>
+        </Space>
+      )
+    },
+  ], []);
+
+  useEffect(() => {
+    getUsers();
 
     return () => {
       setUsers([]);
